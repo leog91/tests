e8b978f7cacae473172176072d8783287770395a
leog91 <leog91@gmail.com>
2016-09-22 20:55:03 -0300

    first com

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lplApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the lplApp
+ */
+angular.module('lplApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name lplApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the lplApp
+ */
+angular.module('lplApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
